f18746820c8ac8e223f36308b6547e6bbc24eae7
joer <joer@sunsidegames.com>
2013-01-18 17:35:19 -0700

    Fixed player bounding box. Radiance updates.
    Signed-off-by: joer <joer@sunsidegames.com>

--- Source/Scripts/Player.lua
+++ Source/Scripts/Player.lua
@@ -14,8 +14,8 @@ function Player.Spawn(self)
 	self.model:BlendToState("manipulate_idle")
 	self:AttachDrawModel(self.model)
 	self:SetDrawModelScale(self.model, {0.4, 0.4, 0.4}) -- temp art
-	self:SetMins({-24, -24, -48})
-	self:SetMaxs({ 24,  24,  48})
+	self:SetMins({-24, -24, -48+64})
+	self:SetMaxs({ 24,  24,  48+64})
 	self:SetDrawModelBounds(self.model, self:Mins(), self:Maxs())
 	self:SetCameraShift({0, 0, 70}) -- camera looks here
 	

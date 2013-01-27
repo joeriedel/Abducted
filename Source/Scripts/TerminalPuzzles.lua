5e889a9ddbbec4fad69333e2bf78acb3f793bf0c
David Reese <davidjreese@yahoo.com>
2013-01-27 09:57:41 -0600

    most of the systems are alpha

--- Source/Scripts/TerminalPuzzles.lua
+++ Source/Scripts/TerminalPuzzles.lua
@@ -21,8 +21,8 @@ function TerminalPuzzles.CreateLevel1x1(self)
 	
 	level.name = "1x1"
 	
-	level.antivirusSpiderSpawnRate = 25
-	level.antivirusSpiderSpeed = 4
+	level.antivirusSpiderSpawnRate = 5
+	level.antivirusSpiderSpeed = 8
 	
 	level.goal = { "symbol_a", "symbol_b", "symbol_c", "symbol_d" }
 
@@ -173,6 +173,7 @@ function TerminalPuzzles.InitUI(self)
 	self.state.gameOver = false
 	self.state.currentMove = 1
 	self.state.gameOverTimer = 5	
+	self.state.victory = false
 	self.state.level = level
 	self.state.spawnTimer = level.antivirusSpiderSpawnRate	
 	self.state.antivirusSpawnTimer = level.antivirusSpiderSpawnRate
@@ -184,6 +185,7 @@ function TerminalPuzzles.InitUI(self)
 	self.widgets.board = { }		
 	self.widgets.lines = { }
 	self.widgets.spiders = { }
+	self.widgets.grid = { }
 	self.widgets.root = UI:CreateWidget("Widget", {rect=UI.fullscreenRect, OnInputEvent=UI.EatInput})
 	World.SetRootWidget(TerminalPuzzles.UI_Layer, self.widgets.root)
 	self.widgets.border = UI:CreateWidget("MatWidget", {rect={0,0,UI.screenWidth,UI.screenHeight}, material=self.gfx.border})
@@ -209,7 +211,7 @@ function TerminalPuzzles.InitUI(self)
 		local index = self:ConvertCoordToIndex(v.x,v.y)
 		b.state = self:CreateState(v.img)
 		self.widgets.root:AddChild(b)
-		self.widgets.board[index] = b		
+		self.widgets.grid[index] = b		
 		self:SetPositionByGrid(b,v.x,v.y)
 		if (v.img == "mark_start") then
 			local current = UI:CreateWidget("MatWidget", {rect={200,200,	self.REFLEX_CELL_SIZE,self.REFLEX_CELL_SIZE}, material=self.gfx.mark_current})		
@@ -217,9 +219,9 @@ function TerminalPuzzles.InitUI(self)
 			self.widgets.current = current
 			self.widgets.root:AddChild(current)
 			self:SetPositionByGrid(current,v.x,v.y)	
-			self.state.playerIndex = index
 			current.state.startPos = self:GetPosition(current) 
 			current.state.endPos = current.state.startPos
+			table.insert(self.widgets.lines,current)
 			self:SetLineSegmentPosition(current,current.state.startPos,current.state.endPos)			
 			COutLine(kC_Debug,"current widget: x=%i, y=%i",v.x,v.y)	
 		end
@@ -298,7 +300,7 @@ function TerminalPuzzles.LoadMaterials(self)
 	self.gfx.symbol_b = World.Load("Reflex-Game/reflex-symbol-b_M")
 	self.gfx.symbol_c = World.Load("Reflex-Game/reflex-symbol-c_M")
 	self.gfx.symbol_d = World.Load("Reflex-Game/reflex-symbol-d_M")
-
+	
 	self.typefaces = {}
 	self.typefaces.BigText = World.Load("UI/TerminalPuzzlesBigFont_TF")				
 end
@@ -317,6 +319,7 @@ end
 
 function TerminalPuzzles.Vec2Normal(self,x,y)
 	local n = math.sqrt(x * x + y * y)
+	--COutLine(kC_Debug,"Vec2Normal: x=%.02f, y=%.02f, n = %f",x,y,n)	
 	local vec2 = { }
 	vec2.x = 0
 	vec2.y = 0
@@ -356,19 +359,19 @@ function TerminalPuzzles.LerpWidget(self,widget,heading,dt,speed)
 	local o = self:LerpVec2(wv,heading,dt,speed)
 
 	if (o.x < half_width) then
-		wv.x = 0
+		o.x = 0
 	end
-	if (wv.x + width > UI.screenWidth) then
-		wv.x = UI.screenWidth - half_width
+	if (o.x + width > UI.screenWidth) then
+		o.x = UI.screenWidth - half_width
 	end
-	if (wv.y < half_height) then
-		wv.y = 0
+	if (o.y < half_height) then
+		o.y = 0
 	end	
-	if (wv.y + height > UI.screenHeight) then
-		wv.y = UI.screenHeight - half_height
+	if (o.y + height > UI.screenHeight) then
+		o.y = UI.screenHeight - half_height
 	end
 	
-	return wv
+	return o
 end
 
 function TerminalPuzzles.GetGridCellFromVec2(self,v)
@@ -485,6 +488,58 @@ function TerminalPuzzles.SetLineSegmentPosition(self,line,startPos,endPos)
 	line:SetRect(r)
 end
 
+function TerminalPuzzles.CollideWithLine(self,x,y)
+	for i, k in pairs(self.widgets.lines) do
+	end
+end
+
+function TerminalPuzzles.CollideWithBoard(self,x,y,isPlayer)
+	if (x < self.COORD_MIN_X) then
+		COutLine(kC_Debug,"CollideWihtBoard found min X @ x=%i, y=%i",x,y)		
+		return true
+	end
+	if (x >= self.COORD_MAX_X) then
+		COutLine(kC_Debug,"CollideWihtBoard found max X @ x=%i, y=%i",x,y)			
+		return true
+	end
+	if (y < self.COORD_MIN_Y) then
+		COutLine(kC_Debug,"CollideWihtBoard found min Y @ x=%i, y=%i",x,y)			
+		return true
+	end		
+	if (y >= self.COORD_MAX_Y) then
+		COutLine(kC_Debug,"CollideWihtBoard found max Y @ x=%i, y=%i",x,y)			
+		return true
+	end	
+	
+	local screenCoord = { }
+	screenCoord.x = x
+	screenCoord.y = y
+	local v = self:GetGridCellFromVec2(screenCoord)
+	local index = self:ConvertCoordToIndex(v.x,v.y)
+	
+	local piece = self.widgets.grid[index]		
+	if (piece == nil) then
+		return false
+	end
+	
+	if (isPlayer) then
+		if (piece.state.architype == "mark_end" or piece.state.architype == "mark_start") then
+			return false
+		end
+		
+		if (piece.state.architype == "cell_a" 
+			or piece.state.architype == "cell_b" 
+			or piece.state.architype == "cell_c" 
+			or piece.state.architype == "cell_d" 
+		) then
+			return false
+		end
+	end
+	
+	COutLine(kC_Debug,"CollideWihtBoard found Piece @ x=%i, y=%i, type=%s",x,y,piece.state.architype)			
+	return true
+end
+
 function TerminalPuzzles.Think(self,dt)
 	-- TODO: -djr, -joe this should be in seconds but is currently in milliseconds (fix in main branch)
 	dt = dt / 1000
@@ -493,15 +548,20 @@ function TerminalPuzzles.Think(self,dt)
 		self.state.gameOverTimer = self.state.gameOverTimer - dt
 		if (self.state.gameOverTimer < 0) then
 			self.state.gameOverTimer = 0
-			self.gameCompleteCallback();
+			--self.gameCompleteCallback();
+			return
 		end
 		
 		if (self.widgets.bigTextLabel == nil) then 
-			self.widgets.bigTextLabel = UI:CreateWidget("TextLabel", {rect={0, 0, 0, 0}, typeface=self.typefaces.BigText})
+			self.widgets.bigTextLabel = UI:CreateWidget("TextLabel", {rect={0, 0, UI.screenWidth,UI.screenHeight}, typeface=self.typefaces.BigText})
 			self.widgets.root:AddChild(self.widgets.bigTextLabel)		
 		end
 		--self.widgets.bigTextLabel:SetText(string.fromat("%i",self.state.gameOverTimer))
-		self.widgets.bigTextLabel:SetText("Game Over")
+		if (self.state.victory) then
+			self.widgets.bigTextLabel:SetText("You Win!")
+		else
+			self.widgets.bigTextLabel:SetText("You Lose!")
+		end
 		UI:CenterLabel(self.widgets.bigTextLabel)
 				
 		-- game over never advances past here
@@ -522,63 +582,56 @@ function TerminalPuzzles.Think(self,dt)
 		self.widgets.current.heading = self.state.heading
 	end
 
-	local currentPos = self:LerpVec2(self.widgets.current.state.endPos,self.state.heading,dt,self.PLAYER_SPEED)
-	--COutLine(kC_Debug,"currentPos: x=%.02f, y=%.02f",currentPos.x,currentPos.y)	
-	local currentGrid = self:GetGridCellFromVec2(currentPos)
-	--COutLine(kC_Debug,"currentGrid: x=%i, y=%i",currentGrid.x,currentGrid.y)	
-	if (self:IsGridCellOnBoard(currentGrid.x,currentGrid.y)) then
-		--COutLine(kC_Debug,"isOnBoard @ currentGrid: x=%i, y=%i",currentGrid.x,currentGrid.y)
-		currentPos = self:ConstrainPointToBoard(currentPos.x,currentPos.y)
-		self.widgets.current.state.endPos = currentPos
-		self:SetLineSegmentPosition(self.widgets.current,self.widgets.current.state.startPos,self.widgets.current.state.endPos)
-		
-		-- detect change of direction
-		if (self.state.lastHeading.x ~= self.state.heading.x or self.state.lastHeading.y ~= self.state.heading.y) then
-			COutLine(kC_Debug,"newLineSegment @ currentGrid: x=%i, y=%i",currentPos.x,currentPos.y)
-			local line = UI:CreateWidget("MatWidget", {rect={200,200,self.REFLEX_CELL_SIZE,self.REFLEX_CELL_SIZE}, material=self.gfx.mark_line_v})
-			line.state = self:CreateState("mark_line_v")
-			self.widgets.lines[self.state.playerIndex] = line
-			self.widgets.current = line
-			self.widgets.root:AddChild(line)		
-						
-			line.state.startPos = currentPos
-			line.state.endPos = line.state.startPos
-			self:SetLineSegmentPosition(line,line.state.startPos,line.state.endPos)
-			
-			self.state.lastHeading.x = self.state.heading.x
-			self.state.lastHeading.y = self.state.heading.y
-		end
-		
---[[
-		UI:CenterWidget(self.widgets.current,currentPos.x,currentPos.y)	
-
-		if (self.widgets.lines[self.state.playerIndex] == nil) then	
-			COutLine(kC_Debug,"line added: x=%i, y=%i",currentGrid.x,currentGrid.y)	
-			-- TODO: -djr change this out with a rectangle stretch. create a new rect on a direction change
-			local line = UI:CreateWidget("MatWidget", {rect={200,200,self.REFLEX_CELL_SIZE,self.REFLEX_CELL_SIZE}, material=self.gfx.mark_line_v})
-			line.state = self:CreateState("mark_line_v")
-			self.widgets.root:AddChild(line)		
-			self.widgets.lines[self.state.playerIndex] = line
-			self:SetPositionByGrid(line,currentGrid.x,currentGrid.y)
-		end
-]]		
-	end		
+	local currentPos = self:LerpVec2(self.widgets.current.state.endPos,self.state.lastHeading,dt,self.PLAYER_SPEED)
+	if (self:CollideWithBoard(currentPos.x,currentPos.y,true)) then
+		COutLine(kC_Debug,"GameOver player collided with board @ : x=%i, y=%i",currentPos.x,currentPos.y)			
+		self.state.gameOver = true
+		return
+	end
 	
-	local pieceAtPlayer = self.widgets.board[self.state.playerIndex]	
-	if (pieceAtPlayer) then
-		COutLine(kC_Debug,"pieceAtPlayer found")
+	--COutLine(kC_Debug,"currentPos: x=%.02f, y=%.02f",currentPos.x,currentPos.y)	
+	currentPos = self:ConstrainPointToBoard(currentPos.x,currentPos.y)
+	self.widgets.current.state.endPos = currentPos
+	self:SetLineSegmentPosition(self.widgets.current,self.widgets.current.state.startPos,self.widgets.current.state.endPos)
+	
+	-- detect change of direction
+	if (self.state.lastHeading.x ~= self.state.heading.x or self.state.lastHeading.y ~= self.state.heading.y) then
+		local oldR = self.widgets.current:Rect()
+		COutLine(kC_Debug,"oldLineSegment @ x=%i, y=%i, width=%i, height=%i",oldR[1],oldR[2],oldR[3],oldR[4])			
+		COutLine(kC_Debug,"newLineSegment @ currentPos: x=%i, y=%i",currentPos.x,currentPos.y)
+		local line = UI:CreateWidget("MatWidget", {rect={200,200,self.REFLEX_CELL_SIZE,self.REFLEX_CELL_SIZE}, material=self.gfx.mark_line_v})
+		line.state = self:CreateState("mark_line_v")
+		table.insert(self.widgets.lines,line)
+		self.widgets.current = line
+		self.widgets.root:AddChild(line)		
+					
+		line.state.startPos = currentPos
+		line.state.endPos = line.state.startPos
+		self:SetLineSegmentPosition(line,line.state.startPos,line.state.endPos)
 		
+		self.state.lastHeading.x = self.state.heading.x
+		self.state.lastHeading.y = self.state.heading.y
+	end
+	
+	local playerGridCell = self:GetGridCellFromVec2(currentPos)
+	local playerIndex = self:ConvertCoordToIndex(playerGridCell.x,playerGridCell.y)	 
+	
+	local pieceAtPlayer = self.widgets.grid[playerIndex]	
+	if (pieceAtPlayer) then		
 		if (self.state.goalCounter < #self.widgets.goals) then
 			local goalPiece = self.widgets.goals[self.state.goalCounter]
 			if (goalPiece.state.architype == pieceAtPlayer.state.architype) then
-				COutLine(kC_Debug,"Goal accomplished: x=%s",gaolPiece.state.architype)	
+				COutLine(kC_Debug,"Goal accomplished: x=%s",goalPiece.state.architype)	
 				self.state.goalCounter = self.state.goalCounter + 1
+				-- TODO: -djr Question: How do they want to indicate that you activated a box, just
+				-- swap out the cell with another cell?				
 			end
 		end		
 		
-		if (pieceAtPlayer.state.architype == "mark_end" and self.state.currentGoal > #self.state.goals) then
+		if (pieceAtPlayer.state.architype == "mark_end" and self.state.goalCounter >= #self.widgets.goals) then
 			COutLine(kC_Debug,"Game Over Detected")
 			self.state.gameOver = true
+			self.state.victory = true
 			return
 		end		
 	end
@@ -598,23 +651,23 @@ function TerminalPuzzles.Think(self,dt)
 		if (spider.state.heading.x == 0 and spider.state.heading.y == 0) then -- failsafe
 			spider.state.heading.x = 1
 		end
-		COutLine(kC_Debug,"spawnedSpider @ currentGrid: x=%i, y=%i, heading = %.04f,%.04f",x,y,spider.state.heading.x,spider.state.heading.y)
+		COutLine(kC_Debug,"spawnedSpider @ grid: x=%i, y=%i, heading = %.04f,%.04f",x,y,spider.state.heading.x,spider.state.heading.y)
 	end	
 	
 	--COutLine(kC_Debug,"Spider move")
-	for i,v in ipairs(self.widgets.spiders) do	
-		local pos = v:Rect()
-		--COutLine(kC_Debug,"pos @ : x=%i, y=%i",pos[1],pos[2])			
-		local nextPos = self:LerpWidget(v,v.state.heading,dt,self.state.level.antivirusSpiderSpeed)
-		local nextGrid = self:GetGridCellFromVec2(nextPos)
-		if (not self:IsGridCellOnBoard(nextGrid.x,nextGrid.y)) then
+	for i,k in pairs(self.widgets.spiders) do	
+		local pos = k:Rect()
+		COutLine(kC_Debug,"pos @ : x=%i, y=%i, dt=%.04f, heading = %.04f,%.04f, speed=%i",pos[1]+pos[3]/2,pos[2]+pos[4]/2,dt,k.state.heading.x,k.state.heading.y,self.state.level.antivirusSpiderSpeed)			
+		local nextPos = self:LerpWidget(k,k.state.heading,dt,self.state.level.antivirusSpiderSpeed)
+		if (self:CollideWithBoard(nextPos.x,nextPos.y,false)) then
 			table.remove(self.widgets.spiders,i)
-			self.widgets.root:RemoveChild(v)
+			self.widgets.root:RemoveChild(k)
 			COutLine(kC_Debug,"remove spider @ : x=%i, y=%i",nextPos.x,nextPos.y)			
 		else
-			UI:CenterWidget(v,nextPos.x,nextPos.y)
+			UI:CenterWidget(k,nextPos.x,nextPos.y)						
 			--COutLine(kC_Debug,"move spider to: x=%.02f, y=%.02f",nextPos.x,nextPos.y)		
 			-- TODO: detect spider crossing a line segment
+
 		end	
 	end	
 end
